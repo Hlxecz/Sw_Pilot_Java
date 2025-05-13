d2a9866afb568002a5e38daef24c7d94b0120f58
HyeungJun <azaz3769@gmail.com>
2025-05-13 22:24:43 +0900

    Revert "Update RoadToBiodome02.java"
    This reverts commit a895f854013f05889e564c72bd219456597d470e.

--- Chapter_2/Step2/src/RoadToBiodome02.java
+++ Chapter_2/Step2/src/RoadToBiodome02.java
@@ -1,12 +1,5 @@
 public class RoadToBiodome02 {
     public static void main(String[] args) {
         System.out.println("장형준");
-        ㄷㄹㅈㅈㄷㄹㅈㄷ
-                ㅁㄴㄹㅈㄷㄹㅈㄷ
-                ㅈㄷㄹㅈㄷㄹㅈㄷㄹ
-                        ㅈㄷㄹㅈㄷㄹ
-                        ㅈㄷㄹ
-                                ㅈㄷㄹㅈㄷㄹ
-                                
     }
 }
