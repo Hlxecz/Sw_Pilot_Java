a895f854013f05889e564c72bd219456597d470e
HyeungJun <azaz3769@gmail.com>
2025-05-13 22:24:16 +0900

    Update RoadToBiodome02.java

--- Chapter_2/Step2/src/RoadToBiodome02.java
+++ Chapter_2/Step2/src/RoadToBiodome02.java
@@ -1,5 +1,12 @@
 public class RoadToBiodome02 {
     public static void main(String[] args) {
         System.out.println("장형준");
+        ㄷㄹㅈㅈㄷㄹㅈㄷ
+                ㅁㄴㄹㅈㄷㄹㅈㄷ
+                ㅈㄷㄹㅈㄷㄹㅈㄷㄹ
+                        ㅈㄷㄹㅈㄷㄹ
+                        ㅈㄷㄹ
+                                ㅈㄷㄹㅈㄷㄹ
+                                
     }
 }
